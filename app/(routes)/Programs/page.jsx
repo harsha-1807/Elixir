c8ac59fe10930d21f3ff21779d0101e8d1f62ce2
harsha-1807 <harshavardhan.sr@kalvium.community>
2024-05-04 23:59:03 +0530

    minor change

--- app/(routes)/Programs/page.jsx
+++ app/(routes)/Programs/page.jsx
@@ -9,14 +9,10 @@ function Programs() {
   const [change, setchange] = useState(0);
   function handleclick(e) {
     setchange(e);
-    
   }
   
   console.log(change)
-  // useEffect(() => {
-  //   handleclick(change)
-   
-  // },[change])
+
   
   return (
     <div className="text-center w-screen h-screen mt-5 ">
@@ -28,9 +24,9 @@ function Programs() {
         
       <div className="md:flex md:gap-14 md:justify-between  bg my-8 text-[1.5rem] w-[20vw]">
         {" "} 
-        <h2 onClick={()=>handleclick(0)}>Creative</h2>
-        <h2 onClick={()=>handleclick(1)} > Core</h2>
-        <h2 onClick={()=>handleclick(2)}>Placement Services</h2>{" "}
+        <h2 onClick={()=>handleclick(1)}  role="button"> Core</h2>
+        <h2 onClick={()=>handleclick(0)} role="button">Creative</h2>
+        <h2 onClick={()=>handleclick(2)} role="button">Placement Services</h2>{" "}
       </div>
       </div>
 
